463f4a17e068eb6a17b11ce08146274bd58ebfaa
eiricvellinga <83020206+eiricvellinga@users.noreply.github.com>
2021-06-30 14:18:48 +0200

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [capstonedjango-ev.eu-gb.mybluemix.net,capstonenginx-ev.eu-gb.cf.appdomain.cloud]
+ALLOWED_HOSTS = ['localhost',capstonedjango-ev.eu-gb.mybluemix.net,capstonenginx-ev.eu-gb.cf.appdomain.cloud]
 
 
 # Application definition
